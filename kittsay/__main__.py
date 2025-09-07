50a2cf19770a75b17a46cd143fe2c02a72a5e49d
ppfeiler <pfeiler.patrick@live.de>
2025-09-07 11:21:55 +0200

    feat(cli): add language parsing and argument handling
    - Introduced a CLI interface for the Kittsay app.
    - Added argument parsing for name and language using argparse.
    - Integrated pycountry for validating ISO 639-1 language codes.
    - Updated run function to accept parameters for name and language.

--- kittsay/__main__.py
+++ kittsay/__main__.py
@@ -1,4 +1,45 @@
+import argparse
+
+import pycountry
+
 from .app import run
 
+
+def parse_language(code: str) -> str:
+    code = code.strip().lower()
+
+    lang = pycountry.languages.get(alpha_2=code)
+    if lang:
+        return lang.name
+
+    raise argparse.ArgumentTypeError(
+        f"Ungültiger Sprachcode: {code}! Nur ISO 639-1 Sprachcodes sind erlaubt. Zum Beispiel: 'de', 'en'",
+    )
+
+
+def cli():
+    parser = argparse.ArgumentParser(
+        prog="Kittsay",
+        description="Kittsay app",
+    )
+
+    parser.add_argument(
+        *["-n", "--name"],
+        type=str,
+        help="Der Name, an den KITT spricht",
+    )
+    parser.add_argument(
+        "-l",
+        "--lang",
+        type=parse_language,
+        default="en",
+        help="Sprache als ISO 639-1 Code (z. B. 'de', 'en')",
+    )
+
+    args = parser.parse_args()
+
+    run(args.name, args.lang)
+
+
 if __name__ == "__main__":
-    run()
+    cli()
--- kittsay/app.py
+++ kittsay/app.py
@@ -1,2 +1,2 @@
-def run() -> None:
-    print("Hello World from kittsay")
+def run(name: str = "", lang: str = "en") -> None:
+    print(f"I should answer to {name} in {lang}")
